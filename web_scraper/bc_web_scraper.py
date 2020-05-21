d47af66a636fa5ecfc26d00b532a43cf270f97a0
Brittney <3944682+bawilc01@users.noreply.github.com>
2020-05-21 02:57:16 -0400

    config to use chrome browser; set lists to store prices

--- web_scraper/bc_web_scraper.py
+++ web_scraper/bc_web_scraper.py
@@ -2,12 +2,25 @@
 
 # Packages imported for web scraping
 # Had to install them all for the project
+# also had to upgrade pip
+
 from selenium import webdriver
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 import pandas as pd
 
 
-URLtoScrape = "https://www.newegg.com/Laptops-Notebooks/Category/ID-223?Tpk=laptops"
+# Configuring selenium webdriver to use the Chrome Browser
+# There are similar commands for other browsers if you wish to use IE, Firefox, Safari, etc.
+driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+
+
+# selenium will open a browser for us to the URL we provide (the newegg url for laptops)
+#
+products=[] #List to store name of the product
+prices=[] #List to store price of the product
+
+driver.get("<a href="https://www.newegg.com/Laptops-Notebooks/Category/ID-223?Tpk=laptops")
+
 
 # Data to scrape
     # Item Name and Price
